a14ce333a16cde8329c08829e2193e93aca44ca9
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 18:20:19 +0300

    redirecting user

--- akram-list/pages/404.js
+++ akram-list/pages/404.js
@@ -1,6 +1,16 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            router.push('/')
+        },3000)
+    } , []);
+
   return (
     <div className="not-found">
       <h1>Ooops...</h1>
