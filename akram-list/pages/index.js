cf9c3b13c0a28cabd8bbc05d6d6badb5d580d7ef
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 17:22:26 +0300

    Update index.js

--- akram-list/pages/index.js
+++ akram-list/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <h1>Home Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <Link href="/list">All List</Link>
+      <Link href="/list">See all List</Link>
       <Footer />
     </div>
   )
