7bd28c8e15c2decb1884960cd965a2fb58422279
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 22:46:54 +0300

    Fetching data

--- akram-list/pages/list/index.js
+++ akram-list/pages/list/index.js
@@ -1,7 +1,25 @@
-const AkramList = () => {
+import styles from '@/styles/list.module.css'
+
+export const getStaticProps = async()=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return{
+        props:{ users:data}
+    }
+}
+
+const AkramList = ({users}) => {
     return ( 
         <div>
             <h1>All Listing</h1>
+            {users.map(user=>(
+                <div key={user.id}>
+                    <a className={styles.single}>
+                        <h3> {user.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
