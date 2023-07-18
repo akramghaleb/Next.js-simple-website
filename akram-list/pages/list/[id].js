72e7a4f75b2e05959d4ea180dcc4c720a0e3f532
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-07-18 23:10:26 +0300

    Fetching a single item

--- akram-list/pages/list/[id].js
+++ akram-list/pages/list/[id].js
@@ -1,7 +1,36 @@
-const Details = () => {
+export const getStaticPaths = async()=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map(user=>{
+        return {
+            params : {id: user.id.toString()}
+        }
+    })
+
+    return{
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async(context)=>{
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id);
+    const data = await res.json();
+
+    return{
+        props:{ user:data}
+    }
+}
+
+const Details = ({user}) => {
     return ( 
         <div>
-            <h1>Details</h1>
+            <h1>{user.name}</h1>
+            <p>{user.email}</p>
+            <p>{user.website}</p>
+            <p>{user.address.city}</p>
         </div>
      );
 }
